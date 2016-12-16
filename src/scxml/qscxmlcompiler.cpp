caed693d21b6e949de326137861908322e92e9f0
Thiago Macieira <thiago.macieira@intel.com>
2016-12-16 14:03:37 -0800

    Remove unnecessary Q_OBJECT_CHECK macro
    It isn't used by qobject_cast anymore since Qt 5.2.
    
    Change-Id: I15b62e0f9cec482fbb40fffd1490dbadff4c5e2a
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/scxml/qscxmlcompiler.cpp
+++ src/scxml/qscxmlcompiler.cpp
@@ -496,8 +496,6 @@ class DynamicStateMachine: public QScxmlStateMachine, public QScxmlInternal::Gen
     Q_DECLARE_PRIVATE(DynamicStateMachine)
     // Manually expanded from Q_OBJECT macro:
 public:
-    Q_OBJECT_CHECK
-
     const QMetaObject *metaObject() const Q_DECL_OVERRIDE
     { return d_func()->m_metaObject; }
 
