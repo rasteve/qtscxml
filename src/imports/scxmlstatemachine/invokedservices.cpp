96a69e7d368a77f52f647fd71c978f418c6b5bac
Ulf Hermann <ulf.hermann@qt.io>
2020-05-19 10:55:30 +0200

    Only return uniquely named services from invokedServices()
    Previously, we used the fact that QVariantMap is in fact a multimap in
    order to squeeze services with equal names into it. This doesn't work
    anymore.
    
    [ChangeLog][Important Behavior Changes] If you have multiple equally
    named children of an invoked service, only one of them will be included
    in the "children" property now. Before, the fact that QVariantMap was
    actually a multimap was used to squeeze all of them in.
    
    Change-Id: I12aa5dda18629a8319c425fad1b9e82e0083befe
    Reviewed-by: Karsten Heimrich <karsten.heimrich@qt.io>

--- src/imports/scxmlstatemachine/invokedservices.cpp
+++ src/imports/scxmlstatemachine/invokedservices.cpp
@@ -73,7 +73,7 @@ QVariantMap QScxmlInvokedServices::children()
     if (m_stateMachine) {
         const QVector<QScxmlInvokableService *> children = m_stateMachine->invokedServices();
         for (QScxmlInvokableService *service : children)
-            ret.insertMulti(service->name(), QVariant::fromValue(service));
+            ret.insert(service->name(), QVariant::fromValue(service));
     }
     return ret;
 }
