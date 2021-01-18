b789cfdd4fb7a833e426072393a4cb23328a1641
Joerg Bornemann <joerg.bornemann@qt.io>
2021-01-18 19:22:12 +0100

    Re-generate all CMake files
    Change-Id: I40dd28da4ba47bd15471b471138900cd22b4e409
    Reviewed-by: Cristian Adam <cristian.adam@qt.io>

--- src/scxml/configure.cmake
+++ src/scxml/configure.cmake
@@ -14,8 +14,11 @@
 
 #### Features
 
-qt_feature("scxml_ecmascriptdatamodel" PUBLIC
+qt_feature("scxml-ecmascriptdatamodel" PUBLIC
     SECTION "SCXML"
     LABEL "ECMAScript data model for QtScxml"
     PURPOSE "Enables the usage of ecmascript data models in SCXML state machines."
 )
+qt_configure_add_summary_section(NAME "Qt Scxml")
+qt_configure_add_summary_entry(ARGS "scxml-ecmascriptdatamodel")
+qt_configure_end_summary_section() # end of "Qt Scxml" section
