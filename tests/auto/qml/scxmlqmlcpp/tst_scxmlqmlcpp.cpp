418cf1a0b9d35d76740e5405c245ce2fb30687bd
Lucie Gérard <lucie.gerard@qt.io>
2022-06-13 16:46:36 +0200

    Use SPDX license identifiers
    Replace the current license disclaimer in files by
    a SPDX-License-Identifier.
    License files are organized under LICENSES directory.
    
    Pick-to: 6.4
    Task-number: QTBUG-67283
    Change-Id: Ic435d3a484be01542f27431068419a2c252bd1f6
    Reviewed-by: Jörg Bornemann <joerg.bornemann@qt.io>

--- tests/auto/qml/scxmlqmlcpp/tst_scxmlqmlcpp.cpp
+++ tests/auto/qml/scxmlqmlcpp/tst_scxmlqmlcpp.cpp
@@ -1,30 +1,5 @@
-/****************************************************************************
-**
-** Copyright (C) 2021 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the QtScxml module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:GPL-EXCEPT$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** GNU General Public License Usage
-** Alternatively, this file may be used under the terms of the GNU
-** General Public License version 3 as published by the Free Software
-** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
-** included in the packaging of this file. Please review the following
-** information to ensure the GNU General Public License requirements will
-** be met: https://www.gnu.org/licenses/gpl-3.0.html.
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
+// Copyright (C) 2021 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
 #include "../../shared/util.h"
 
@@ -178,7 +153,7 @@ void tst_scxmlqmlcpp::stateMachineLoaderSourceStateMachineBinding()
         sourceBrokenNoScheme = sourceBroken.toLocalFile();
     QTest::ignoreMessage(QtWarningMsg,
                          qPrintable(smlUrl.toString() + ":5:5: QML StateMachineLoader: " +
-                        sourceBrokenNoScheme + ":59:1: error: initial state 'working' " +
+                        sourceBrokenNoScheme + ":12:1: error: initial state 'working' " +
                         "not found for <scxml> element"));
 
     QTest::ignoreMessage(QtWarningMsg,
