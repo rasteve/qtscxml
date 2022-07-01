2961ac00e77ff782e195d98cf873b41eca7f96a3
Joerg Bornemann <joerg.bornemann@qt.io>
2022-07-01 11:38:44 +0200

    qt6_add_statecharts: Deduplicate code
    Change-Id: I27162e28cc61dd771425ba6a226ade6e65ad5e99
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- src/scxml/Qt6ScxmlMacros.cmake
+++ src/scxml/Qt6ScxmlMacros.cmake
@@ -46,8 +46,8 @@ function(qt6_add_statecharts target_or_outfiles)
         get_filename_component(outfilename ${it} NAME_WE)
         get_filename_component(infile ${it} ABSOLUTE)
         set(outfile ${qscxmlcOutputDir}/${outfilename})
-        set(outfile_cpp ${qscxmlcOutputDir}/${outfilename}.cpp)
-        set(outfile_h ${qscxmlcOutputDir}/${outfilename}.h)
+        set(outfile_cpp ${outfile}.cpp)
+        set(outfile_h ${outfile}.h)
 
         add_custom_command(OUTPUT ${outfile_cpp} ${outfile_h}
                            ${qscxmlc_command} ${namespace} ${ARGS_OPTIONS}
