dbe330d7e0018e0045a62b78d4ef918a29cd596c
Ulf Hermann <ulf.hermann@qt.io>
2024-12-02 16:14:45 +0100

    Adapt to QQmlCustomParser signature change
    We cannot expose executable compilation units in verifyBindings()
    because verifyBindings() is called from the type loader thread. However,
    we actually only need the base compilation units anyway.
    
    Pick-to: 6.8
    Change-Id: I990a6e00caeba3f3e6cb76921225bc1d0aa66a4b
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- src/statemachineqml/signaltransition_p.h
+++ src/statemachineqml/signaltransition_p.h
@@ -86,8 +86,12 @@ private:
 class SignalTransitionParser : public QQmlCustomParser
 {
 public:
-    void verifyBindings(const QQmlRefPointer<QV4::ExecutableCompilationUnit> &compilationUnit, const QList<const QV4::CompiledData::Binding *> &props) override;
-    void applyBindings(QObject *object, const QQmlRefPointer<QV4::ExecutableCompilationUnit> &compilationUnit, const QList<const QV4::CompiledData::Binding *> &bindings) override;
+    void verifyBindings(
+            const QQmlRefPointer<QV4::CompiledData::CompilationUnit> &compilationUnit,
+            const QList<const QV4::CompiledData::Binding *> &props) override;
+    void applyBindings(
+            QObject *object, const QQmlRefPointer<QV4::ExecutableCompilationUnit> &compilationUnit,
+            const QList<const QV4::CompiledData::Binding *> &bindings) override;
 };
 
 template<>
